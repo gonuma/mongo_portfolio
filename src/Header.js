46aa71db0b119a0589b353b55b6fe1894972feb2
Greg Edmondson <fallabrine@gmail.com>
2023-01-11 03:03:26 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./no-back-logo.svg";
 import { Navbar, Nav, NavLink, Container } from "react-bootstrap";
 
 const Header = () => {
