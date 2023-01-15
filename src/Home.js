faff16baa0bedb8d23567f93cf0a969981b3b9c8
gonuma <fallabrine@gmail.com>
2023-01-15 12:46:33 +0900

    Slight update to about me page

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
 
 const Home = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <Container style={{ backgroundColor: "pink" }}>
+    <Container fluid style={{ backgroundColor: "white", height: "94.8vh" }}>
       <Row
         style={{
           padding: "1vh",
-          backgroundColor: "gray",
+          backgroundColor: "#D3D3D3",
+          height: "23vh",
+          marginLeft: "1vw",
+          marginRight: "1vw",
         }}
       >
-        <Col></Col>
+        <Col xs={2}>
+          {/* <Image
+            fluid
+            style={{
+              borderRadius: "25px",
+              maxHeight: "20vh",
+            }}
+            src="https://s3.ap-northeast-1.amazonaws.com/www.gonuma.com/images/cowboy.JPG"
+          /> */}
+        </Col>
         <Col
-          xs={{ span: 8 }}
+          xs={8}
           style={{
-            // alignSelf: "center",
             textAlign: "center",
           }}
         >
           <Row>
             <h1>Greg Edmondson</h1>
-          </Row>
-          <Row>
             <h5>IT Professional</h5>
-          </Row>
-          <Row>
             <h5>📧 GregEdmondson95@gmail.com | ☎️ 080-7725-0495</h5>
-          </Row>
-          <Row>
             <h5>🏠 Chofu, Tokyo, Japan</h5>
           </Row>
         </Col>
@@ -39,6 +51,7 @@ const Home = () => {
             fluid
             style={{
               borderRadius: "25px",
+              maxHeight: "20vh",
             }}
             src="https://s3.ap-northeast-1.amazonaws.com/www.gonuma.com/images/cowboy.JPG"
           />
@@ -47,26 +60,87 @@ const Home = () => {
       <Row
         style={{
           padding: "1vh",
+          backgroundColor: "black",
         }}
       >
-        <Col style={{ fontStyle: "italic", textAlign: "center" }}>
+        <Col
+          style={{ color: "white", fontStyle: "italic", textAlign: "center" }}
+        >
           The best time to plant a tree was 20 years ago. The second best time
           is now.
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <Image
-            fluid
-            style={{
-              // height: "50vh",
-              // padding: "1vh",
-              borderRadius: "25px",
-              // display: "block",
-              // margin: "auto",
-            }}
-            src="https://s3.ap-northeast-1.amazonaws.com/www.gonuma.com/images/cowboy.JPG"
-          />
+      <Row
+        fluid
+        style={{
+          backgroundColor: "#D3D3D3",
+          height: "23vh",
+          padding: "1vh",
+          marginLeft: "1vw",
+          marginRight: "1vw",
+        }}
+      >
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <Col
+            // style={{ margin: "auto" }}
+            >
+              Having moved to Japan at the age of 20, I have worked in a number
+              of industries. I began my journey in Japan as many do, as an
+              English teach in a rural city. However, the whole time I was
+              yearning to move to the big city and work in the videogame
+              industry. After searching high and low, I ended up finding a
+              position in Tokyo, working as a videogame translator. It was a
+              dream come true to be wrapped in the heart of the gaming world.
+              However, after several years of working in the videogame industry,
+              I began to feel the itch for change, for a challenge. I had always
+              loved computers, having built a few for myself and friends, and
+              being the designated IT-guy in the family. In 2021, I decided to
+              delve deeper into this interest in tech and enrolled in an
+              intensive programming bootcamp known as Code Chrysalis. Not only
+              had I never written a single line of code before, but my son had
+              just been born a few months prior. I had hopes that this would set
+              me on a new path in my career and life, but I didn't quite realize
+              just how difficult, and rewarding, this choice would be.
+            </Col>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Col
+            // style={{ margin: "auto" }}
+            >
+              Code Chrysalis was a flurry of learning. I had never written a
+              single line of code before, and here I was taking a course to
+              learn full-stack programming. Every day we were studying new
+              technologies, including data-structures, GraphQL, SQL databases,
+              React, Node, containers, and a little sprinkle of Ruby for good
+              measure.
+            </Col>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row
+        style={{
+          padding: "1vh",
+          backgroundColor: "black",
+        }}
+      >
+        <Col style={{ color: "white", textAlign: "center" }}>
+          <h1>Projects</h1>
+        </Col>
+      </Row>
+      <Row
+        fluid
+        style={{
+          backgroundColor: "#D3D3D3",
+          height: "23vh",
+          padding: "1vh",
+          marginLeft: "1vw",
+          marginRight: "1vw",
+        }}
+      >
+        <Col style={{ margin: "auto" }}>
+          I'm going to use this space to introduce a few projects that I've
+          previously worked on.
         </Col>
       </Row>
     </Container>
