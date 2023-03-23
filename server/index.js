1992fb369c47e2b2275527fe758927e08afe4e87
gonuma <fallabrine@gmail.com>
2023-03-23 22:02:24 +0900

    Updated Music page

--- server/index.js
+++ server/index.js
@@ -2,22 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cors = require("cors")
+const cors = require("cors");
 const mongoose = require("mongoose");
 const Article = require("./model/article");
 const request = require("request");
 
-
 //CORS Settings
 //app.use(cors());
-app.use(function(req, res, next){
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-res.setHeader("Access-Control-Allow-Credentials", true);
-next();
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
-app.use(cors({origin:"*", credentials: true}))
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 const port = 5000;
@@ -26,62 +25,63 @@ const port = 5000;
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 // Get default MongoDB connection
 const db = mongoose.connection;
 
 // Bind connection to error event
 db.once("open", () => {
-    console.log("Connected to MongoDB database.")
-})
+  console.log("Connected to MongoDB database.");
+});
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // Fetch all articles
 app.get("/articles", async (req, res) => {
-    const articles = await Article.find({});
-    // console.log("Articles from DB: ", articles);
-    res.send(articles);
-})
+  const articles = await Article.find({});
+  // console.log("Articles from DB: ", articles);
+  res.send(articles);
+});
 
 // Fetch single article
 app.get("/article", async (req, res) => {
-    const article = await Article.find({});
-    // console.log("Selected article: ", article);
-    res.send(article);
-})
+  const article = await Article.find({});
+  // console.log("Selected article: ", article);
+  res.send(article);
+});
 
 // Create a new article
 app.post("/article", async (req, res) => {
-    try {
-        console.log("req.body: ", req.body);
+  try {
+    console.log("req.body: ", req.body);
 
-        const newArticle = new Article ({
-            title: req.body.title,
-            body: req.body.body,
-            category: req.body.category
-        });
+    const newArticle = new Article({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category,
+    });
 
-        await Article.create(newArticle);
-        res.send("Article added");
+    await Article.create(newArticle);
+    res.send("Article added");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-    } catch (err) {
-        console.log(err);
-    }
-})
+// Generate Access Token
+app.get("/refresh", async (req, res) => {
+  let parameters = {
+    body: `grant_type=refresh_token&refresh_token=${process.env.SPOTIFY_REFRESH_TOKEN}`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + process.env.SPOTIFY_ID_SECRET_64,
+    },
+  };
 
-// Fetch Spotify top artists
-const headers = {Accept:"application/json", "content-type":"application/json", Authorization:process.env.SPOTIFY_API_AUTH}
-const url = "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5"
-
-app.get("/artists", async (req, res) => {
-        request.get({url: url, headers: headers}, function (e, r, body) {
-            console.log(body)
-            res.send(body)
-        })
-})
-
-
+  fetch("https://accounts.spotify.com/api/token", parameters)
+    .then((response) => response.json())
+    .then((data) => res.send(data));
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- src/Music.js
+++ src/Music.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -7,64 +7,107 @@ import Carousel from "react-bootstrap/Carousel";
 import axios from "axios";
 
 const Music = () => {
-  
-  const [artists, setArtists] = useState([])
-  
-  const loadSongs = async () => {
+  const [artists, setArtists] = useState([]);
+  const [refreshToken, setRefreshToken] = useState("");
+  const [isLoading, setLoading] = useState(true);
+  let token = [refreshToken[0]];
+
+  // Load Refreshed Token
+  const getRefreshToken = async () => {
     const response = await axios.get(
       window.location.protocol +
-      "//" +
-      window.location.hostname +
-      ":5000/artists"
+        "//" +
+        window.location.hostname +
+        ":5000/refresh"
     );
-    setArtists(response.data.items);
+    setRefreshToken(response.data.access_token);
+  };
+
+  // Load Top Artists
+  const loadArtists = async () => {
+    await fetch(
+      "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10&offset=5",
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + refreshToken,
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => setArtists(data.items))
+      .then(setLoading(false));
   };
 
   useEffect(() => {
-    loadSongs();
+    getRefreshToken();
   }, []);
-
-
-
-  return (
-    <Container >
-      {/* <Image
-        fluid
+  useEffect(() => {
+    loadArtists();
+  }, token);
+  if (isLoading) {
+    return (
+      <div
         style={{
-          maxHeight: "50vh",
-          maxWidth: "90vw",
-          padding: "1vh",
-          display: "block",
-          margin: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-        src="https://s3.ap-northeast-1.amazonaws.com/www.gonuma.com/images/jimmy-page-robert-plant-freddie-mercury.jpg"
-      /> */}
-<Row>
-
-      <Carousel>
-<Carousel.Item><div></div></Carousel.Item>
-        {
-          artists.map((artist=> {
-            console.log(artist.name)
-            console.log(artist.images[2].url)
-            return (
-              <Carousel.Item >
-                
-                <Col fluid className="align-items-center" style={{ backgroundColor: "lightblue"}}>
-              <Image style={{height:"90vh"}} src={artist.images[0].url}/>
-              {/* <Carousel.Caption>{artist.name}</Carousel.Caption> */}
-                </Col>
-          </Carousel.Item>
-          )
-        }))
-        
-      }
-      <Carousel.Item><div></div></Carousel.Item>
-      </Carousel>
-      </Row>
-      {/* <button onClick={()=>console.log(artists)}>test</button> */}
-    </Container>
-  );
+      >
+        <h1>Loading...</h1>;
+      </div>
+    );
+  } else if (artists && artists.length > 0) {
+    return (
+      <Container>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>Greg's Top Artists...</h1>
+        </div>
+        <Row>
+          <Carousel>
+            {artists.map((artist) => {
+              console.log(artist.name);
+              console.log(artist.images[2].url);
+              return (
+                <Carousel.Item>
+                  <Col
+                    fluid
+                    className="align-items-center"
+                    style={{
+                      maxHeight: "50vh",
+                      maxWidth: "90vw",
+                      padding: "1vh",
+                      display: "block",
+                      margin: "auto",
+                    }}
+                  >
+                    <Image
+                      style={{
+                        display: "flex",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        height: "50vh",
+                      }}
+                      src={artist.images[0].url}
+                    />
+                    <Carousel.Caption>{artist.name}</Carousel.Caption>
+                  </Col>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default Music;
